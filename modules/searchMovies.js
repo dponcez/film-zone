15bc5f834aa02c4db0661c4c3808f47bb55069c5
Damian Ponce <dponcez_git@hotmail.com>
2025-09-01 21:41:23 -0500

    feat: enhance movie search functionality with improved result handling

---
+++ modules/searchMovies.js
@@ -0,0 +1,45 @@
+import { selector, handler } from '../_fns/custom-functions.js';
+import { fetchMovie } from './fetchMovies.js';
+import { debounce } from '../utils/debounce.js';
+
+/**
+ * Search movies by name
+ * 
+ * @param { function } reloadFn: function to reload the page (optional, for testing)
+ * @param { string } search_movie_url: base URL for movie search
+  * @example 
+  * ('https://api.../search?query=') 
+*/
+
+export const searchMovies = (search_movie_url, reloadFn = () => window.location.reload()) => {
+  const search = selector('[data-search]');
+  const form = selector('[data-form]');
+  const main = selector('[data-main]');
+
+  const movieFinder = async (query) => {
+    const url = await fetchMovie(`${search_movie_url}${query}`);
+    const filteredResult = url.filter(movie => movie.title.toLowerCase().include(query.toLowerCase()));
+
+    return new Promise(resolve => {
+      debounce(() => resolve(filteredResult), 300)
+    });
+  }
+
+  const displayResults = async (event) => {
+    const query = event.target.value.trim();
+    if(!query) reloadFn();
+
+    try{
+      if(query && query.length !== ''){
+        await movieFinder()
+      }else{
+        main.innerHTML = `<p class="info">No movies found!</p>`
+      }
+    }catch(error){
+      main.innerHTML = `<p style="color: hsl(11, 89%, 37%)" class="info">Error fetching movies: ${error.message}</p>`;
+    }
+  }
+
+  handler(search, 'input', displayResults)
+  handler(form, 'submit', (event) => event.preventDefault())
+}
