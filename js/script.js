1abe0bbfebfeafb12641b3f179f337c26c5beea9
Damian Ponce <dponcez_git@hotmail.com>
2025-09-04 21:07:31 -0500

    feat: update fetchMovie and searchMovie calls with local URLs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import { fetchMovie } from '../modules/fetchMovies.js';
 import { searchMovie } from '../modules/searchMovies.js';
 
-fetchMovie();
-searchMovie()
+const local_movie_url = 'http://localhost:3000/movies';
+const local_search_url = 'http://localhost:3000/search';
+
+fetchMovie(local_movie_url);
+searchMovie(local_search_url);
